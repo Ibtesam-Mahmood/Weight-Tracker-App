911cf6109e31d8b638e436242b30d25ef4579915
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-02-23 15:27:54 -0500

    Obtains reference to the text

--- app/src/main/java/com/example/ibtes/weighttrainingapp/MainActivity.java
+++ app/src/main/java/com/example/ibtes/weighttrainingapp/MainActivity.java
@@ -3,16 +3,29 @@ package com.example.ibtes.weighttrainingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.inputText);
     }
 
+    //Takes the inputted test and adds it as a recorded option
     public void onClickButton(View v){
+        String currentText =  editText.getText().toString();
+
+        if(currentText == "")
+            return;
+
+        editText.setText("");
+
         
     }
 }
