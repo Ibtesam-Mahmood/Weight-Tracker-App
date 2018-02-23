3fd75add609439ffff40d066db5ed1ffdff5b2da
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-02-23 15:36:55 -0500

    Adds the text that layout

--- app/src/main/java/com/example/ibtes/weighttrainingapp/MainActivity.java
+++ app/src/main/java/com/example/ibtes/weighttrainingapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.ibtes.weighttrainingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,18 +17,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.inputText);
+        editText = (EditText) findViewById(R.id.inputText);
     }
 
     //Takes the inputted test and adds it as a recorded option
     public void onClickButton(View v){
+        //Obtains the text within the input dialog
         String currentText =  editText.getText().toString();
 
+        //Insures the logic only runs if the edit text was not initially empty
         if(currentText == "")
             return;
 
-        editText.setText("");
+        editText.setText(""); //Resets the edit text
 
-        
+        LinearLayout layout = (LinearLayout) findViewById(R.id.textLayout); //Refrences the linear layout
+
+        //Sets the parameters for the new text
+        ViewGroup.LayoutParams params =  new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+
+        TextView newText =  new TextView(this);
+        newText.setText(currentText); //Sets the text to the inputted text
+        newText.setLayoutParams(params); //Sets the parameters for the text view
+
+        layout.addView(newText); //Adds the text t the layout
+
     }
 }
