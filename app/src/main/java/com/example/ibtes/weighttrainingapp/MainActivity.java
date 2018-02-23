e24452b94701e0ce3dce84fe811aa93765033256
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-02-23 14:51:15 -0500

    Code initialized

---
+++ app/src/main/java/com/example/ibtes/weighttrainingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ibtes.weighttrainingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
